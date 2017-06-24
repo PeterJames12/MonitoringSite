9d6a569b72f1fde7ecbb16287f9b2beaf5783f75
peterjames <joyukr>
2017-06-24 14:04:11 +0300

    fixed database

--- src/main/java/monitoring/config/DatabaseConfig.java
+++ src/main/java/monitoring/config/DatabaseConfig.java
@@ -1,19 +1,35 @@
 package monitoring.config;
 
-import lombok.Cleanup;
 import lombok.SneakyThrows;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 /**
  * @author Igor Hnes on 6/24/17.
  */
 public class DatabaseConfig {
 
-    private static final String URL = "jdbc:postgresql://ec2-54-75-229-201.eu-west-1.compute.amazonaws.com:5432/d8h0drjb9eoidt?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
-    private static final String USERNAME = "cmienhftbssytm";
-    private static final String PASS = "3d33063c06d2d1a20bec8b1c099836b8245bbbdb9f9e2587cacb3802e699046e";
+    private static final String URL = "jdbc:postgresql://ec2-23-23-93-255.compute-1.amazonaws.com:5432/d5t06k85ohsutd?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+    private static final String USERNAME = "zqrtvxhfqmdjft";
+    private static final String PASS = "5946f5d3a5434dc18a5fd32dc6759c9b62b37cb0e687cb7e1f80b27dbd09c7ba";
+
+    public static void main(String[] args) throws SQLException {
+
+        final Connection connection = getConnection();
+        final Statement statement = connection.createStatement();
+//        statement.execute("CREATE TABLE public.url\n" +
+//                "(\n" +
+//                "    id SERIAL PRIMARY KEY NOT NULL,\n" +
+//                "    name VARCHAR(255)\n" +
+//                ");");
+
+
+        final ResultSet resultSet = statement.executeQuery("select * from url;");
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name"));
+        }
+    }
 
     @SneakyThrows
     public static Connection getConnection() {
