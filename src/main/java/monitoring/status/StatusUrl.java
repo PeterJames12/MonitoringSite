2004ddb1fc3dda38c5678a7f6fc545445b6bf771
peterjames <joyukr>
2017-06-24 17:29:21 +0300

    added status const

---
+++ src/main/java/monitoring/status/StatusUrl.java
@@ -0,0 +1,11 @@
+package monitoring.status;
+
+/**
+ * @author Igor Hnes on 6/24/17.
+ */
+public class StatusUrl {
+
+    public static final String OK = "OK";
+    public static final String WARNING = "WARNING";
+    public static final String CRITICAL = "CRITICAL";
+}
